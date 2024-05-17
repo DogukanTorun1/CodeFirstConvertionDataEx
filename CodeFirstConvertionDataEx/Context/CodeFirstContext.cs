86ec34d899372ec044dd16b0f280e015eaefc1f3
DogukanTorun1 <ddogukan97@gmail.com>
2024-05-17 20:18:30 +0300

    First Migration Created

---
+++ CodeFirstConvertionDataEx/Context/CodeFirstContext.cs
@@ -0,0 +1,36 @@
+﻿using CodeFirstConvertionDataEx.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstConvertionDataEx.Context
+{
+    public class CodeFirstContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Employment> Employments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer("Server=.;Database=CodeFirstDB1;Trusted_Connection=True;TrustServerCertificate=true");
+
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Employment)
+                .WithOne(e => e.Employee)
+                .HasForeignKey<Employment>(e => e.EmployeeId);
+        }
+
+
+
+    }
+}
